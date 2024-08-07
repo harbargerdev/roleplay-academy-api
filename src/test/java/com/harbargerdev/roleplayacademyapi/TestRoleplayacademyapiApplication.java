62f2c6142ad9ec15df795d357d02c814860ae817
harbargerdev <aharbarger@gmail.com>
2024-08-06 23:29:34 -0400

    Fixing some tests

--- src/test/java/com/harbargerdev/roleplayacademyapi/TestRoleplayacademyapiApplication.java
+++ src/test/java/com/harbargerdev/roleplayacademyapi/TestRoleplayacademyapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestRoleplayacademyapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(RoleplayAcademyApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.run(RoleplayAcademyApiApplication.class, "test");
 	}
 
 }
