ef5a042d245b5d27b211c6d512c4e3e1836819fc
harbargerdev <codeamber0621@pm.me>
2025-09-21 21:08:17 -0400

    Added missing profile setting for test scope

--- src/test/java/com/harbargerdev/roleplayacademyapi/controllers/HelloWorldControllerTests.java
+++ src/test/java/com/harbargerdev/roleplayacademyapi/controllers/HelloWorldControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(HelloWorldController.class)
 @AutoConfigureMockMvc(addFilters = false) // Disable security filters for testing purposes
+@ActiveProfiles("test")
 public class HelloWorldControllerTests {
 
     @Autowired
--- src/test/java/com/harbargerdev/roleplayacademyapi/controllers/ProtectedControllerTests.java
+++ src/test/java/com/harbargerdev/roleplayacademyapi/controllers/ProtectedControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(ProtectedController.class)
+@ActiveProfiles("test")
 public class ProtectedControllerTests {
 
     @Autowired
